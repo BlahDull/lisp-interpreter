4cbeb99cb836d8e51d77ab7516cde8ade937b3c5
BlahDull <91094497+BlahDull@users.noreply.github.com>
2024-03-29 17:16:18 -0600

    Added functionality for interpreting arithmatic expressions for +, -, *, and /

---
+++ lisp.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cctype>
+#include <unordered_map>
+
+using namespace std;
+
+// (+ (+ 3 4) 5)
+
+enum Tokens {
+    PLUS = '+',
+    MINUS = '-',
+    MULTIPLY = '*',
+    DIVIDE = '/',
+    LPAREN = '(',
+    RPAREN = ')',
+    NOP = '_'
+};
+
+class Token {
+    public:
+        unsigned char token_val = NOP;
+        string number_val = "";
+        string variable_name = "";
+        bool is_number = false;
+        bool is_varaible = false;
+
+        Token(unsigned char input) {
+            token_val = input;
+        }
+        
+        Token(string input) {
+            if (contains_number(input)) {
+                number_val = input;
+                is_number = true;
+            } else {
+                variable_name = input;
+                is_varaible = true;
+            }
+        }
+    private:
+        bool contains_number(string input) {
+            for (char c : input) {
+                if (!isdigit(c) && c != '.') {
+                    return false;
+                }
+            }
+            return true;
+        }
+};
+
+class Lexer {
+    public:
+        vector<Token> tokens;
+        void tokenize(string input_str) {
+            for (size_t i = 0; i < input_str.length(); i++) {
+                if (input_str.at(i) == ' ') continue;
+                else if (input_str.at(i) == '+') {
+                    tokens.push_back(Token(PLUS));
+                }
+                else if (input_str.at(i) == '-') {
+                    tokens.push_back(Token(MINUS));
+                }
+                else if (input_str.at(i) == '*') {
+                    tokens.push_back(Token(MULTIPLY));
+                }
+                else if (input_str.at(i) == '/') {
+                    tokens.push_back(Token(DIVIDE));
+                }
+                else if (input_str.at(i) == '(') {
+                    tokens.push_back(Token(LPAREN));
+                }
+                else if (input_str.at(i) == ')') {
+                    tokens.push_back(Token(RPAREN));
+                }
+                else if (isdigit(input_str.at(i))) {
+                    stringstream ss;
+                    string num;
+                    while (isdigit(input_str.at(i))) {
+                        ss << input_str.at(i);
+                        i++;
+                    }
+                    i--;
+                    ss >> num;
+                    tokens.push_back(Token(num));
+                } else {
+                    stringstream ss;
+                    string term;
+                    while (input_str.at(i) != RPAREN && input_str.at(i) != ' ') {
+                        ss << input_str.at(i);
+                        i++;
+                    }
+                    i--;
+                    ss >> term;
+                    tokens.push_back(Token(term));
+                }
+            }
+        }
+};
+
+void print_tokens(vector<Token> *tokens) {
+    cout << "[ ";
+    for (Token token : *tokens) {
+        if (!token.is_number) {
+            cout << "'" << token.token_val << "' ";
+        } else {
+            cout << "'" << token.number_val << "' ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+class Parser {
+    public:
+        void parse(vector<Token> tokens) {
+            while(!tokens.empty()) {
+                string result = parse_expression(&tokens).number_val;
+                cout << truncate_zeros(result) << endl;
+            }
+        }
+
+    private:
+        unordered_map<string, int> symbol_table;
+        vector<string> keywords = {"set!", "define", "car", "cdr", "cons", "sqrt", "pow", "defun", "if"};
+
+        Token eat(vector<Token> *vec) {
+            Token token = *vec->begin();
+            if (!vec->empty()) {
+                vec->erase(vec->begin());
+            }
+            return token;
+        }
+
+        Token look_ahead(vector<Token> *vec) {
+            if (vec->size() > 1) {
+                Token token = vec->at(1);
+                return token;
+            } else {
+                return Token(NOP);
+            }
+        }
+
+        void push(vector<Token> *vec, Token token) {
+            vec->insert(vec->begin(), token);
+        }
+
+        Token parse_expression(vector<Token> *tokens) {
+            Token token = eat(tokens);
+            if (token.is_number) {
+                return token;
+            }
+            if (token.token_val == LPAREN) {
+                while (token.token_val != RPAREN) {
+                    token = eat(tokens);
+                    unsigned char val = token.token_val;
+                    switch (val) {
+                        case PLUS: {
+                            float operand1 = stof(parse_expression(tokens).number_val);
+                            float operand2 = stof(parse_expression(tokens).number_val);
+                            token = eat(tokens);
+                            if (!token.is_number) {
+                                return Token(to_string(operand1 + operand2));
+                            } else {
+                                push(tokens, token);
+                                push(tokens, Token(to_string(operand1 + operand2)));
+                                push(tokens, Token(PLUS));
+                            }
+                            break;
+                        }
+                        case MINUS: {
+                            float operand1 = stof(parse_expression(tokens).number_val);
+                            float operand2 = stof(parse_expression(tokens).number_val);
+                            token = eat(tokens);
+                            if (!token.is_number) {
+                                return Token(to_string(operand1 - operand2));
+                            } else {
+                                push(tokens, token);
+                                push(tokens, Token(to_string(operand1 - operand2)));
+                                push(tokens, Token(MINUS));
+                            }
+                            break;
+                        }
+                        case MULTIPLY: {
+                            float operand1 = stof(parse_expression(tokens).number_val);
+                            float operand2 = stof(parse_expression(tokens).number_val);
+                            token = eat(tokens);
+                            if (!token.is_number) {
+                                return Token(to_string(operand1 * operand2));
+                            } else {
+                                push(tokens, token);
+                                push(tokens, Token(to_string(operand1 * operand2)));
+                                push(tokens, Token(MULTIPLY));
+                            }
+                            break;
+                        }
+                        case DIVIDE: {
+                            float operand1 = stof(parse_expression(tokens).number_val);
+                            float operand2 = stof(parse_expression(tokens).number_val);
+                            if (operand2 == 0) {
+                                cout << "Attempted division by 0. Aborting...";
+                                tokens->clear();
+                                return Token(NOP);
+                            }
+                            token = eat(tokens);
+                            if (!token.is_number) {
+                                return Token(to_string(operand1 / operand2));
+                            } else {
+                                push(tokens, token);
+                                push(tokens, Token(to_string(operand1 / operand2)));
+                                push(tokens, Token(DIVIDE));
+                            }
+                            break;
+                        }
+
+                        case LPAREN: {
+                            push(tokens, parse_expression(tokens));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        string truncate_zeros(string str) {
+            int index = str.find(".");
+            for (size_t i = index + 1; i < str.size(); i++) {
+                if (str.at(i) != '0') {
+                    return str;
+                }
+            }
+            return str.substr(0, index);
+        }
+};
+
+
+int main()
+{
+    string input;
+    cout << "Welcome to the Lisp Interpreter. Type in Lisp Commands!" << endl;
+    while (1) {
+        //getline(cin, input);
+        input = "(+ (+ (+ 5 3) 2) (- 4 3))";
+        //input = "(/ 20 6 2)";
+        if (input == "bye") break;
+        Lexer lexer = Lexer();
+        lexer.tokenize(input);
+        print_tokens(&lexer.tokens);
+        Parser parser = Parser();
+        parser.parse(lexer.tokens);
+        break;
+    }
+    return 0;
+}
